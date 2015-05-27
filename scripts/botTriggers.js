0a522b5e11661e9eec5c558f1f6ac3d1397db22a
Toph Tucker <tophtucker@gmail.com>
2015-05-27 03:24:40 -0400

    Sloppy bot triggers: scrolling too fast, pick up where you left off

--- scripts/botTriggers.js
+++ scripts/botTriggers.js
@@ -11,9 +11,33 @@ jQuery(document).ready(function($) {
         localStorage.getItem('visitCount') +
         " times. Remember to look for me throughout the article for moments where you can play along.";
     }
-    paulbot.mode("on");
-    paulbot.speak(message);
-    paulbot.wait(5000).then(function() { paulbot.mode("off"); });
+
+    var dialogue = [
+      {
+        "mode": "on",
+        "speak": message
+      }
+    ];
+    if(localStorage.getItem('scrollTop')) {
+      dialogue.push({
+        "prompts": [
+          {"prompt": "Continue reading from last spot","dialogue": [{
+            "eval": "document.getElementsByTagName('body')[0].scrollTop = parseInt(localStorage.getItem('scrollTop'))"
+          }]},
+          {"prompt": "No thanks"}
+        ]
+      })
+    } else {
+      dialogue.push({
+        "wait": 5000
+      })
+    }
+    dialogue.push({
+      "mode": "off"
+    });
+
+    paulbot.dialogue(dialogue);
+
   }, 500);
 })
 
@@ -33,3 +57,30 @@ $(window).on('scroll', function(e) {
     paulbot.wait(3000).then(function() { paulbot.mode("off"); });
   }
 })
+
+var scrollLog = [];
+setInterval(function() {
+  var scrollTop = document.getElementsByTagName("body")[0].scrollTop;
+  scrollLog.push({
+    "scrollTop": scrollTop,
+    "timestamp": +(new Date())
+  });
+
+  var scrollSpeed =
+    (scrollLog[scrollLog.length-1].scrollTop - scrollLog[scrollLog.length-2].scrollTop) /
+    (scrollLog[scrollLog.length-1].timestamp - scrollLog[scrollLog.length-2].timestamp)
+
+
+  if(scrollSpeed > 4) {
+    paulbot.mode("on");
+    paulbot.emote('troll');
+    paulbot.speak(function() { return "You're scrolling so fast! Sloooowww dowwnnnn!" });
+    paulbot.wait(3000).then(function() { paulbot.mode("off"); });
+  }
+
+}, 1000);
+
+window.onunload = window.onbeforeunload = function(event) {
+  localStorage.setItem('scrollTop', document.getElementsByTagName("body")[0].scrollTop);
+  return;
+}
