a329580ec57fab1272916a2b6b7a7a323f671884
Toph Tucker <tophtucker@gmail.com>
2015-05-27 13:03:57 -0400

    Added a few alert pics

--- modules/crapplets/crapplets.js
+++ modules/crapplets/crapplets.js
@@ -1,9 +1,6 @@
 var crapplets = function(sel) {
 
-  var alerts = ["1.jpg", "13.png", "17.png", "20.jpg", "24.png", "28.jpg", "31.jpg", "35.png", "4.png", "8.jpg",
-    "10.gif", "14.png", "18.png", "21.png", "25.png", "29.jpg", "32.jpg", "36.PNG", "5.jpg", "9.png",
-    "11.png", "15.jpg", "19.jpeg", "22.JPG", "26.png", "3.png", "33.png", "37.jpg", "6.jpg",
-    "12.jpeg", "16.jpg", "2.png", "23.jpg", "27.PNG", "30.png", "34.png", "38.png", "7.png"];
+  var alerts = ["1.jpg", "14.png", "19.jpeg", "23.jpg", "28.jpg", "32.jpg", "37.jpg", "41.png", "7.png 10.gif", "15.jpg", "2.png", "24.png", "29.jpg", "33.png", "38.png", "42.png", "8.jpg  11.png", "16.jpg", "20.jpg", "25.png", "3.png", "34.png", "39.png", "43.png", "9.png  12.jpeg", "17.png", "21.png", "26.png", "30.png", "35.png", "4.png", "5.jpg  13.png", "18.png", "22.JPG", "27.PNG", "31.jpg", "36.PNG", "40.png", "6.jpg"];
 
   var alertIntervals = [];
   var container;
