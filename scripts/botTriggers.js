2a7a3b2c9819d00a6dac853fd05730c60c4802e1
Toph Tucker <tophtucker@gmail.com>
2015-05-28 01:56:05 -0400

    God I suck at programming. Bot triggers, time on page.

--- scripts/botTriggers.js
+++ scripts/botTriggers.js
@@ -1,4 +1,6 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(botTriggers);
+
+function botTriggers($) {
   setTimeout(function() {
     if(localStorage.getItem('visitCount') == 1) {
       var message = "Hey, welcome" +
@@ -39,64 +41,76 @@ jQuery(document).ready(function($) {
     paulbot.dialogue(dialogue);
 
   }, 500);
-})
 
-var hasTriggered = false;
-$(window).on('scroll', function(e) {
+  var hasTriggered = false;
+  $(window).on('scroll', function(e) {
 
-  // when you hit the bottom of the page
-  if($(window).scrollTop() + $(window).height() > $(document).height() - 100 && !hasTriggered) {
+    // when you hit the bottom of the page
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100 && !hasTriggered) {
 
-    hasTriggered = true;
+      hasTriggered = true;
 
-    var bottomTime = new Date();
-    var timeDiff = (((bottomTime - loadTime) / 1000 / 60)*100).toFixed()/100;
+      var bottomTime = new Date();
+      var timeDiff = (((bottomTime - loadTime) / 1000 / 60)*100).toFixed()/100;
+      paulbot.mode("on");
+      paulbot.emote('troll');
+      paulbot.speak(function() { return "So you read 38,000 words in " + timeDiff + " minutes." });
+      paulbot.wait(3000).then(function() { paulbot.mode("off"); });
+    }
+  })
+
+  var scrollLog = [];
+
+  var fastSass = _.shuffle([
+    "You're scrolling too fast! Sloooowww dowwnnnn!",
+    "Wow you can read so quickly!",
+    "This is like a zillion wpm.",
+    "Are you reading my article or are you looking at my article.",
+    "Hey Barbecue, where's the fire?",
+    "Trying to skip to the bottom?",
+    "Are you just looking for fancy Snowfally things to jump out at you? Read more words. They're good.",
+    "Excuse me, my words are up here."
+  ]);
+
+  var throttler = $.throttle(6000, alertTooFast);
+
+  $(window).on("scroll", $.throttle(1000, logScroll));
+  logScroll();
+
+  function alertTooFast() {
     paulbot.mode("on");
     paulbot.emote('troll');
-    paulbot.speak(function() { return "Oh so you read 38,000 words in only " + timeDiff + " minutes, did you? Stop and smell the roses." });
+    paulbot.speak(function() { return fastSass.pop(); });
     paulbot.wait(3000).then(function() { paulbot.mode("off"); });
   }
-})
 
-var scrollLog = [];
-var fastSass = _.shuffle([
-  "You're scrolling so fast! Sloooowww dowwnnnn!",
-  "Wow you can read so quickly!",
-  "This is like a zillion wpm.",
-  "Are you reading my article or are you looking at my article.",
-  "Hey Barbecue, where's the fire?",
-  "Trying to skip to the bottom?",
-  "Are you just looking for fancy Snowfally things to jump out at you? Read more words. They're good.",
-  "Excuse me, my words are up here."
-]);
-var throttler = $.throttle(6000, alertTooFast);
-function alertTooFast() {
-  console.log("alerting too fast...");
-  paulbot.mode("on");
-  paulbot.emote('troll');
-  paulbot.speak(function() { return fastSass.pop(); });
-  paulbot.wait(3000).then(function() { paulbot.mode("off"); });
-}
-setInterval(function() {
-  var scrollTop = document.getElementsByTagName("body")[0].scrollTop;
-  scrollLog.push({
-    "scrollTop": scrollTop,
-    "timestamp": +(new Date())
-  });
+  function logScroll() {
+    var scrollTop = document.getElementsByTagName("body")[0].scrollTop;
+    scrollLog.push({
+      "scrollTop": scrollTop,
+      "timestamp": +(new Date())
+    });
 
-  var scrollSpeed =
-    (scrollLog[scrollLog.length-1].scrollTop - scrollLog[scrollLog.length-2].scrollTop) /
-    (scrollLog[scrollLog.length-1].timestamp - scrollLog[scrollLog.length-2].timestamp)
+    if(scrollLog.length < 2) return;
 
-  console.log("logging scroll...");
-  if(scrollSpeed > 4 && fastSass.length) {
-    console.log("too fast...");
-    throttler();
+    var scrollSpeed =
+      (scrollLog[scrollLog.length-1].scrollTop - scrollLog[scrollLog.length-2].scrollTop) /
+      (scrollLog[scrollLog.length-1].timestamp - scrollLog[scrollLog.length-2].timestamp)
+
+    if(scrollSpeed > 4 && fastSass.length) {
+      throttler();
+    }
   }
 
-}, 1000);
+  window.onunload = window.onbeforeunload = function(event) {
+    localStorage.setItem('scrollTop', document.getElementsByTagName("body")[0].scrollTop);
 
-window.onunload = window.onbeforeunload = function(event) {
-  localStorage.setItem('scrollTop', document.getElementsByTagName("body")[0].scrollTop);
-  return;
+    var timeOnPage = (+new Date()) - scrollLog[0].timestamp;
+    if(localStorage.getItem('timeOnPage')) {
+      timeOnPage += parseInt(localStorage.getItem('timeOnPage'));
+    }
+    localStorage.setItem('timeOnPage', timeOnPage);
+
+    return;
+  }
 }
