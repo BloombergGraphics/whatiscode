5bb60e6ace14876bae74baf2e972cc799686a77d
Adam Pearce <apearce19@bloomberg.com>
2015-05-29 11:55:57 -0400

    module scroll working for diffs

---
+++ scripts/module-manager.js
@@ -0,0 +1,27 @@
+var addModule = (function(){
+  var modules = []
+
+  calcPositions = _.throttle(function(){
+    modules.forEach(function(d){
+      d.bbox     = d.sel.node().getBoundingClientRect()
+      d.startPos = d.bbox.top    + pageYOffset - 1000
+      d.endPos   = d.bbox.bottom + pageYOffset
+    })
+  }, 1000)
+
+  d3.select(window).on('scroll.module', _.debounce(function(d){
+    modules.forEach(function(d){
+      d.active = d.startPos < pageYOffset && pageYOffset < d.endPos
+    })
+  }, 300))
+
+  d3.select(window).on('resize.module', calcPositions)
+
+  //content on page moves without trigging any resizing...?
+  setInterval(calcPositions, 2000)
+
+  return function(module){
+    modules.push(module)
+    calcPositions()
+  }
+})()
