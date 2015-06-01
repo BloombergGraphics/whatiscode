f5a22b11f50953425b2dac705d524ba46e687450
Toph Tucker <tophtucker@gmail.com>
2015-06-01 00:58:04 -0400

    Refactoring keyboard to use module manager

--- scripts/botDialogues.js
+++ scripts/botDialogues.js
@@ -76,22 +76,6 @@ botDialogues.shoppingCart = [
   }
 ]
 
-botDialogues.keyboardEvents = [
-  {
-    "trigger": "in",
-  },
-  {
-    "speak": "Mash your keys, see the scancodes.",
-    "eval": "keyboardEvents(true)"
-  },
-  {
-    "trigger": "out",
-  },
-  {
-    "eval": "keyboardEvents(false)"
-  }
-]
-
 botDialogues.mouseEvents = [
   {
     "trigger": "in",
