3e3fb3ba4cb43e97aa7c3c6428876558395861ce
Adam Pearce <apearce19@bloomberg.com>
2015-06-03 17:43:15 -0400

    adds backing path

--- modules/circuit/circuit.js
+++ modules/circuit/circuit.js
@@ -106,9 +106,23 @@
 
 
   //add elements to the page
-  var svg = d3.select('#circuit').append('svg')
+  var svgBase = d3.select('#circuit').append('svg')
       .attr({width: width + margin.left + margin.right, height: height + margin.top + margin.bottom})
+  
+  svgBase.append('rect').attr({width: width + margin.left + margin.right, height: height + margin.top + margin.bottom})
+      .style('fill', '#fff')
+
+  var svg = svgBase
     .append('g').translate([margin.left, margin.right])
+
+
+
+
+
+  var wireBotEls = svg.dataAppend(wires, 'path.wire.bot').attr('d', ƒ('pathStr'))
+      .style('stroke', 'lightgrey')
+  var wireTopEls = svg.dataAppend(wires, 'path.wire.top').attr('d', ƒ('pathStr'))
+      .attr('stroke-dasharray', '100% 100%')
 
   var gateGs = svg.dataAppend(gates, 'g')
       .translate(function(d){ return [d.x, d.y] })
@@ -118,11 +132,13 @@
   //     .style({stroke: 'black', fill: 'darkgrey'})
   //     .attr({x: -gS/2, y: -gS/2, width: gS, height: gS})
 
-  gateGs.append('g.gate-icon').selectAll('path')
-    .data(function(d) { return d.type.paths; })
-    .enter()
-      .append('path')
-      .attr('d', function(d) { return d; })
+  gateGs.append('g.gate-icon')
+    .dataAppend(ƒ('type', 'paths'), 'path.background')
+      .attr('d', ƒ())
+
+  gateGs.append('g.gate-icon')
+    .dataAppend(ƒ('type', 'paths'), 'path.foreground')
+      .attr('d', ƒ())
 
   gateGs.append('text')
       .text(ƒ('type', 'str'))
@@ -130,10 +146,6 @@
       .style('font-size', '68%')
 
 
-  var wireBotEls = svg.dataAppend(wires, 'path.wire.bot').attr('d', ƒ('pathStr'))
-      .style('stroke', 'lightgrey')
-  var wireTopEls = svg.dataAppend(wires, 'path.wire.top').attr('d', ƒ('pathStr'))
-      .attr('stroke-dasharray', '100% 100%')
 
 
   //update logic
@@ -154,7 +166,7 @@
     var uuid = Math.random()
 
     gateGs.selectAll('g.gate-icon').filter(ƒ('changedOn'))
-      .transition(uuid).delay(function(d){ return (d.i - i)*300+ 100})
+      .transition(uuid).delay(function(d){ return (d.i - i)*300+ 200})
         .style('fill', color)
         .style('fill-opacity', 1)
       .transition(uuid).duration(300)
