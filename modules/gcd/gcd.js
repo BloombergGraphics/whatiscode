5e6a7a21f8e0ffba89c4aa9b4b4d32e69caf9675
Adam Pearce <apearce19@bloomberg.com>
2015-06-08 13:32:04 -0400

    turns gcd sideways

--- modules/gcd/gcd.js
+++ modules/gcd/gcd.js
@@ -11,9 +11,9 @@
       return v ? gcd(v, u % v) : u
     }
 
-    while (!stack.length || stack.length > 6 || _.last(stack).u < 5){
-      a = 960 - Math.floor(Math.random()*100)
-      b = 400 + Math.floor(Math.random()*100)
+    while (!stack.length || stack.length < 4 || stack.length > 5 || _.last(stack).u < 5){
+      a = 960 - Math.floor(Math.random()*400)
+      b = 400 - Math.floor(Math.random()*300)
       stack = []
       gcd(a, b)
     }
@@ -22,10 +22,12 @@
     window.setTimeout(drawGCD, (stack.length + 3)*stepDelay)
     // if (!module.active) return
 
-    var size = 960,
-        s    = 960/Math.max(a, b),
-        s = 1
+    var size = 300,
+        s    = 300/Math.max(a, b),
+        // s = 1
         colors = colorArray
+
+    console.log(s)
 
     stack[0].translate = [0, 0]
 
@@ -37,14 +39,14 @@
 
       d.prev = i ? stack[i - 1] : d
 
-      d.flipped = !(!i || !(i % 2))
+      d.flipped = (!i || !(i % 2))
       d.x = d.flipped ? d.vs : d.us
       d.y = d.flipped ? d.us : d.vs
 
       d.num = Math.floor(d.u/d.v)
 
       d.translate = d.prev.translate.slice()
-      if (i) d.translate[+!d.flipped] += d.prev.v*d.prev.num
+      if (i) d.translate[+!d.flipped] += d.prev.vs*d.prev.num
       d.color = colors[i]
     })
 
@@ -54,10 +56,51 @@
     numToColor[a] = 'black'
     numToColor[0] = 'lightgrey'
 
-    var svg = d3.select('[data-module="gcd"]').html('').append('svg')
-        .attr({width: size, height: 500})
 
-    var text = d3.select('[data-module="gcd"]').append('div.explanation')
+    var text = d3.select('[data-module="gcd"]').html('').append('div.explanation')
+
+    var svg = d3.select('[data-module="gcd"]').append('svg')
+        .attr({width: size, height: a*s})
+
+
+    svg.append('rect').attr({height: a*s, width: b*s})
+
+    svg.dataAppend(stack, 'g')
+      .each(function(d){
+        if (!d.v) return
+
+        d3.select(this).dataAppend(d3.range(d.num).map(function(i){ return {i: i, j: d.i, p: d} }), 'rect.step')
+            .attr(d.flipped ? 'y' : 'x', function(e){ return e.i*d.vs })
+            .attr('width',  d.vs)
+            .attr('height', d.vs)
+            .style('fill', d.color)
+            .style('stroke', d3.rgb(d.color).darker(2))
+      })
+      .translate(ƒ('translate'))
+
+
+    svg.selectAll('rect.step').style('opacity', 0)
+      .transition().delay(function(d, i){ return d ? (d.j + 1)*stepDelay : 0 })
+        .style('opacity', 1)
+        .style('stroke-width', 10)
+      .transition().duration(500)
+        .style('stroke-width', 1)
+
+
+    // svg.append('text')
+    //     .translate([10, 500/2])
+    //   .dataAppend(stack, 'tspan.number')
+    //     .text(function(d){ return ' ' + d.u + ' ' })
+        
+    //     .style({fill: function(d, i){ return i ? d.prev.color : 'black' }})
+    //     .style('opacity', 0)
+    //   .transition().delay(function(d){ return d.i*stepDelay })
+    //     .style('opacity', 1)
+
+
+
+
+    
 
     var stepDivs = text.dataAppend(stack.concat('last'), 'div')
     stepDivs.dataAppend(function(d, i){
@@ -101,39 +144,6 @@
       .transition().delay(function(d, i){ return (i + 1)*stepDelay })
         .style('opacity', 1)
 
-    svg.append('rect').attr({width: a, height: b})
-
-    svg.dataAppend(stack, 'g')
-      .each(function(d){
-        if (!d.v) return
-
-        d3.select(this).dataAppend(d3.range(d.num).map(function(i){ return {i: i, j: d.i, p: d} }), 'rect.step')
-            .attr(d.flipped ? 'y' : 'x', function(e){ return e.i*d.v })
-            .attr('width',  d.v)
-            .attr('height', d.v)
-            .style('fill', d.color)
-            .style('stroke', d3.rgb(d.color).darker(2))
-      })
-      .translate(ƒ('translate'))
-
-
-    svg.selectAll('rect.step').style('opacity', 0)
-      .transition().delay(function(d, i){ return d ? (d.j + 1)*stepDelay : 0 })
-        .style('opacity', 1)
-        .style('stroke-width', 10)
-      .transition().duration(500)
-        .style('stroke-width', 1)
-
-
-    svg.append('text')
-        .translate([10, 500/2])
-      .dataAppend(stack, 'tspan.number')
-        .text(function(d){ return ' ' + d.u + ' ' })
-        
-        .style({fill: function(d, i){ return i ? d.prev.color : 'black' }})
-        .style('opacity', 0)
-      .transition().delay(function(d){ return d.i*stepDelay })
-        .style('opacity', 1)
 
   }
 
