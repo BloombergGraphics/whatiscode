fb00af1383655c8e8c867109143279813416317b
Collins <kcollins49@bloomberg.net>
2015-06-09 11:36:22 -0400

    code tinder done ready for edits

--- modules/tinder/tinder.js
+++ modules/tinder/tinder.js
@@ -17,7 +17,7 @@
 	var ok = arrowBox.append("div.ok").html("[OK]");
 
 	var pbotHolder = appscreen.append("div.pbot-holder");
-	var pbotGif = pbotHolder.append("img").attr("src","images/emotes/chill.gif")
+	var pbotGif = pbotHolder.append("img").attr("src","images/emotes/explaining2.gif")
 
 	var footer = appscreen.append("div.footer");
 	var name = footer.append("h3");
@@ -33,34 +33,44 @@
 
 	var codeSamples = [
 		{
-			"code": "remotedb.allDocs({\n\tinclude_docs: true,\n\tattachments: true\n}).then(function (result) {\n\tvar docs = result.rows;\n\tdocs.forEach(function(element) {\n\t\tlocaldb.put(element.doc).then(function(response) {\n\t\t\talert(\"Pulled doc with id \" + element.doc._id + \" and added to local db.\");\n\t\t}).catch(function (err) {\n\t\t\tif (err.status == 409) {\n\t\t\t\tlocaldb.get(element.doc._id).then(function (resp) {\n\t\t\t\t\tlocaldb.remove(resp._id, resp._rev).then(function (resp) {",
+			"code": "remotedb.allDocs({\n  include_docs: true,\n  attachments: true\n}).then(function (result) {\n  var docs = result.rows;\n  docs.forEach(function(element) {\n    localdb.put(element.doc).then(function(response) {\n      alert(\"Pulled doc with id \" + element.doc._id + \" and added to local db.\");\n    }).catch(function (err) {\n      if (err.status == 409) {\n        localdb.get(element.doc._id).then(function (resp) {\n          localdb.remove(resp._id, resp._rev).then(function (resp) {",
 			"bio": "Hey girl, I asynchronously fetch records from a remote database and store them locally, while handling errors with aplomb.",
 			"name": "fetchFromServer",
-			"paulbot": "This function manages to keep a lot of promises that ultimately aren't worth much; at that rate, he could've just called me back."
+			"paulbot": "This function manages to keep a lot of promises that ultimately aren't worth much; at that rate, he could've just called me back.",
+			"correctAnswer": 0,
+			"top":180
 		},
 		{
-			"code": "remotedb.allDocs(...).then(function (resultOfAllDocs) {\n\treturn localdb.put(...);\n}).then(function (resultOfPut) {\n\treturn localdb.get(...);\n}).then(function (resultOfGet) {\n\treturn localdb.put(...);\n}).catch(function (err) {\n\tconsole.log(err);\n});",
+			"code": "remotedb.allDocs(...).then(function (resultOfAllDocs) {\n  return localdb.put(...);\n}).then(function (resultOfPut) {\n  return localdb.get(...);\n}).then(function (resultOfGet) {\n  return localdb.put(...);\n}).catch(function (err) {\n  console.log(err);\n});",
 			"bio": "My tidily composed promises are entirely without side effects, and I'll be honest about any errors.",
 			"name": "[Name TK]",
-			"paulbot": "This is so much better-proportioned than the last one."
+			"paulbot": "This is so much better-proportioned than the last one.",
+			"correctAnswer": 1,
+			"top":230
 		},
 		{
-			"code": "function d3_formatPrefix(d, i) {\n\tvar k = Math.pow(10, abs(8 - i) * 3);\n\treturn {\n\t\tscale: i > 8 ? function(d) {\n\t\t\treturn d / k;\n\t\t} : function(d) {\n\t\t\treturn d * k;\n\t\t},\n\t\tsymbol: d\n\t};\n}",
+			"code": "function d3_formatPrefix(d, i) {\n  var k = Math.pow(10, abs(8 - i) * 3);\n  return {\n    scale: i > 8 ? function(d) {\n      return d / k;\n    } : function(d) {\n      return d * k;\n    },\n    symbol: d\n  };\n}",
 			"bio": "Worlds collide! I take primitive numbers the way I understand them and help adapt them to your historical conventions of thousands-grouping and Greek prefixes.",
 			"name": "d3_formatPrefix",
-			"paulbot": "Not that broadly useful and a little enigmatic, but a compact li’l function."
+			"paulbot": "A little enigmatic, but a compact li’l function.",
+			"correctAnswer": 1,
+			"top":230
 		},
 		{
-			"code": "String::getStatements = ->\n\t# ... stuff\n\twhile chars.length > 0\n\t\t# ... stuff\n\t\tor (chars.length is 0)\n\t\t\t# ... stuff\n\t\t\t\t\t\t\t\t\t\tlastCap = null\n\t\t\t\t\t\telse\n\t\t\t\t\t\t\t\tclosing = \".\"\n\t\t\t\t\t\t\t\t\n\t\t\t\telse if (char in QUOTES)\n\t\t\t\t\t\tclosing = char\n\t\tstatements\n\t\t\nSECTION = 'run.coffee'",
+			"code": "String::getStatements = ->\n  # ... stuff\n  while chars.length > 0\n    # ... stuff\n    if (char in QUOTES and closing in QUOTES) \\n    or (char in PUNCTUATION and closing in PUNCTUATION) \\n    or (chars.length is 0)\n      # ... stuff\n      if chars.length is 0 or doBreak\n        if current.length > 0\n          # ... stuff\n          lastCap = null\n      else\n        closing = \".\"\n    else if (char in QUOTES)\n      closing = char\n  statements\n  \nSECTION = 'run.coffee'",
 			"bio": "I’m spontaneous. I like to make plans and throw things together at the last minute. Let’s get messy and see what happens!",
 			"name": "[Name TK]",
-			"paulbot": "See that HTML embedded in the source code? Sure, it gets the job done, but it leaves the door wide open for other things to go wrong."
+			"paulbot": "See that HTML embedded in the source code? Sure, it gets the job done, but it leaves the door wide open for other things to go wrong.",
+			"correctAnswer": 0,
+			"top":190
 		},
 		{
-			"code": "# Invokes interceptor with the obj, and then returns obj.\n# The primary purpose of this method is to \"tap into\" a method chain, in order to perform operations on intermediate results within the chain.\n_.tap = (obj, interceptor) ->\n\tinterceptor obj\n\tobj",
+			"code": "# Invokes interceptor with the obj, and then returns obj.\n# The primary purpose of this method is to \"tap into\" a method chain, in order to perform operations on intermediate results within the chain.\n_.tap = (obj, interceptor) ->\n  interceptor obj\n  obj",
 			"bio": "What you see is what you get. I’m upfront and honest about what I bring to the table.",
 			"name": "[Name TK]",
-			"paulbot": "Ooh! Look at all that clearly documented code! Sexy."
+			"paulbot": "Ooh! Look at all that clearly documented code! Sexy.",
+			"correctAnswer": 1,
+			"top":250
 		}
 	];
 
@@ -102,14 +112,23 @@
 	}
 
 	function makeChoice() {
+		var el = d3.select(this);
+		var cls = el.attr("class");
+		
+		var feedback = "<div class='feedback'>I agree.</div>";
+		if (cls == "button like" && codeSamples[stepCount].correctAnswer == 0) {
+			feedback = "<div class='feedback'>I disagree.</div>"
+		} else if (cls == "button dislike" && codeSamples[stepCount].correctAnswer == 1) {
+			feedback = "<div class='feedback'>I disagree.</div>"
+		}
+
 		d3.selectAll(".selected").classed("selected", false);
-		paulTalk(codeSamples[stepCount].paulbot);
+		paulTalk(feedback+codeSamples[stepCount].paulbot);
 		stepCount = stepCount + 1;
 	}
 
 	// click ok and continue
 	ok.on("click",function(){
-		arrowBox.style("top","150px");
 
 		name.text(codeSamples[stepCount].name);
 		bio.text(codeSamples[stepCount].bio);
@@ -118,6 +137,7 @@
     hljs.highlightBlock(code.node());
 
 		arrowBox.transition().duration(300).style("opacity",0);
+		arrowBox.transition().delay(300).style("top",codeSamples[stepCount].top+"px");
 		pbotHolder.transition().duration(300).style("opacity",0);
 		
 		choiceButtons
