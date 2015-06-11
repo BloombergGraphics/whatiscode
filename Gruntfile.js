8589a6c754c72c16e9fc0072fcecb5d3cc8b0e46
Adam Pearce <apearce19@bloomberg.com>
2015-06-10 23:08:37 -0400

    grunt working

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function (grunt) {
       misc: {
         files: [
           { expand: true, src: 'scripts/tracker.js', dest: 'public/' },
-          { expand: true, cwd: '.tmp/concat/', src: 'app.js', dest: 'public/' },
+          // { expand: true, cwd: '.tmp/concat/', src: 'app.js', dest: 'public/' },
         ]
       }
     }
@@ -55,7 +55,7 @@ module.exports = function (grunt) {
     'useminPrepare',
     'concat',
     'cssmin',
-    // 'uglify',
+    'uglify',
     'copy',
     'usemin',
   ]);
